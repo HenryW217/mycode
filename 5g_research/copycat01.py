d1f3d7484e37e0c05bdc4044224ba4dd20831ac7
HenryW217 <HenryW217@users.noreply.github.com>
2022-08-11 15:31:07 +0000

    Done with Lab 37

---
+++ 5g_research/copycat01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import shutil
+import os
+
+# The following line will create the directory if it does not exist already
+
+os.chdir("/home/student/mycode/")
+#shutil.copy("5g_research/sdn_network.txt", "5g_research/sdn_network.txt.copy")
+#shutil.copytree("5g_research/", "5g_research_backup/")
+
+# just to play with it
+# os.system("rm /home/student/mycode/6g/test1")
+# shutil.rmtree("/home/student/mycode/6g")
+
