2e6b1ffd2bae7d1dad2007331f9afe92c0c04573
HenryW217 <HenryW217@users.noreply.github.com>
2022-08-15 15:23:14 +0000

    done with lab 59

--- walkingtree/firstmatchstop.py
+++ walkingtree/firstmatchstop.py
@@ -7,7 +7,7 @@ def find_all(name, path):
     for root, dirs, files in os.walk(path):
         if name in files:
             result.append(os.path.join(root, name))
-
+    return result
 lookfor = input('What am I looking for? ')
 lookwhere = input('What is the path in which I should search? ')
 
